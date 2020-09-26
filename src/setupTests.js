5adaa478eb5405ff9a98a26631390888f4af36bd
John Otu <johnne01@gmail.com>
2020-09-26 11:51:16 +0100

    chore: DonutChart component
    added a DonutChart component for displaying data in pie chart form.
    
    fixes #17

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+// jest-canvas-mock mocks a canvas element on the DOM to enable charts to render properly during tests.
+import 'jest-canvas-mock';
