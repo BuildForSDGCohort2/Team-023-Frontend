1f142a390e3c7727dbceedf0ffb791575517ed44
John Otu <johnne01@gmail.com>
2020-09-26 14:05:29 +0100

    chore: LineChart component
    added LineChart component for displaying trends in important metrics
    
    fixes #16

---
+++ src/components/LineChart/LineChart.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import LineChart from "./LineChart";
+
+describe("LineChart", () => {
+  test("renders LineChart component", () => {
+    render(<LineChart />);
+  });
+});
